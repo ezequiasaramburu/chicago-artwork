3e74f02e6e0f78d4c4379ea562821d34152bb943
Ezequias Aramburu <ezearamburu@hotmail.com>
2023-03-13 17:12:59 -0300

    add details screen

--- src/navigation/rootStack/index.tsx
+++ src/navigation/rootStack/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {HomeScreen} from 'src/views/HomeScreen';
+import {DetailScreen} from 'src/views/DetailScreen';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -23,6 +24,11 @@ export const RootStack: React.FC = () => {
         name="Home"
         component={HomeScreen}
         options={{title: 'Artworks'}}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailScreen}
+        options={props => ({title: props.route.params?.title})}
       />
     </Stack.Navigator>
   );
