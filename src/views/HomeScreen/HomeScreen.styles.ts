674716158573f2e3b4ccde016d835843304b9304
Ezequias Aramburu <ezearamburu@hotmail.com>
2023-03-13 18:02:44 -0300

    remove unused style

--- src/views/HomeScreen/HomeScreen.styles.ts
+++ src/views/HomeScreen/HomeScreen.styles.ts
@@ -3,7 +3,6 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   flex1: {
     flex: 1,
-    backgroundColor: 'red',
   },
   center: {
     alignItems: 'center',
